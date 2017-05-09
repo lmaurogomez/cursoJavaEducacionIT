f3bb24036d36892a87ec64c7efa30bb733f30cb0
Lucas Mauro Gomez <luesteban94@gmail.com>
2017-05-09 20:45:11 -0300

    Prueba intelij

---
+++ src/clase05/Programa.java
@@ -0,0 +1,16 @@
+package clase05;
+
+
+/**
+ * Created by Lucas on 09/05/2017.
+ */
+public class Programa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo Prueba");
+    }
+
+}
---
+++ src/clase05/Prueba.java
@@ -0,0 +1,13 @@
+package clase05;
+
+/**
+ * Created by Lucas on 09/05/2017.
+ */
+public class Prueba {
+    int p;
+    String hola;
+
+    public Prueba(int p) {
+        this.p = p;
+    }
+}
